e07831deff94580c2a2aab99623c87d7d86fbafe
kinjo <kinjo@barnet.co.jp>
2018-06-12 18:26:02 +0900

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+if [ ! -s "/gitbook/SUMMARY.md" ]; then
+  # create README.md and SUMMARY.md
+  gitbook init /gitbook
+  echo 'Gitbook templete was generated.'
+else
+  gitbook install
+  echo 'Gitbook templete already exists.'
+fi
+
+exec tini -- $@
